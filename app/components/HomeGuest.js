b75c69c1eaff469acee748874011dbe6b17f062e
rcavalcanti <Rafael.Cavalcanti@fit-tecnologia.org.br>
2022-08-30 16:44:19 -0300

    Access form filds

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Page from "./Page";
 import Axios from "axios";
 
 function HomeGuest() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   async function handleSubmit(e) {
     e.preventDefault();
     try {
       await Axios.post("http://localhost:8080/register", {
-        username: "marie",
-        email: "marie@test.com",
-        password: "qwerty123456"
+        username,
+        email,
+        password
       });
       console.log("User was successfully created.");
     } catch (e) {
@@ -36,6 +40,7 @@ function HomeGuest() {
                 <small>Username</small>
               </label>
               <input
+                onChange={e => setUsername(e.target.value)}
                 id="username-register"
                 name="username"
                 className="form-control"
@@ -49,6 +54,7 @@ function HomeGuest() {
                 <small>Email</small>
               </label>
               <input
+                onChange={e => setEmail(e.target.value)}
                 id="email-register"
                 name="email"
                 className="form-control"
@@ -62,6 +68,7 @@ function HomeGuest() {
                 <small>Password</small>
               </label>
               <input
+                onChange={e => setPassword(e.target.value)}
                 id="password-register"
                 name="password"
                 className="form-control"
