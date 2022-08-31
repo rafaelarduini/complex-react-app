dcf58a81e28bb7d3860c54679af41b2738d7fcec
rcavalcanti <Rafael.Cavalcanti@fit-tecnologia.org.br>
2022-08-31 14:27:34 -0300

    What is Immer?

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useImmerReducer } from "use-immer";
 import Axios from "axios";
 Axios.defaults.baseURL = "http://localhost:8080";
 
@@ -24,23 +25,23 @@ function Main() {
     flashMessages: []
   };
 
-  function ourReducer(state, action) {
+  function ourReducer(draft, action) {
     switch (action.type) {
       case "login":
-        return { loggedIn: true, flashMessages: state.flashMessages };
+        draft.loggedIn = true;
+        return;
 
       case "logout":
-        return { loggedIn: false, flashMessages: state.flashMessages };
+        draft.loggedIn = false;
+        return;
 
       case "flashMessage":
-        return {
-          loggedIn: state.loggedIn,
-          flashMessages: state.flashMessages.concat(action.value)
-        };
+        draft.flashMessages.push(action.value);
+        return;
     }
   }
 
-  const [state, dispatch] = useReducer(ourReducer, initialState);
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
 
   return (
     <StateContext.Provider value={state}>
