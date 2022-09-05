12013b160796f93b8a7f02aaa35f79220e4c2812
rcavalcanti <Rafael.Cavalcanti@fit-tecnologia.org.br>
2022-09-05 13:59:20 -0300

    Edit Post Continued

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -48,14 +48,14 @@ function ViewSinglePost() {
       <div className="d-flex justify-content-between">
         <h2>{post.title}</h2>
         <span className="pt-2">
-          <a
-            href="#"
+          <Link
+            to={`/post/${post._id}/edit`}
             data-tip="Edit"
             data-for="edit"
             className="text-primary mr-2"
           >
             <i className="fas fa-edit"></i>
-          </a>
+          </Link>
           <ReactTooltip id="edit" className="custom-tooltip" />{" "}
           <a
             data-tip="Delete"
