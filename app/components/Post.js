d2148d332f624fecac6ce0ba4536c597d5eda6df
rcavalcanti <Rafael.Cavalcanti@fit-tecnologia.org.br>
2022-09-13 15:09:27 -0300

    Homepage Post Feed

---
+++ app/components/Post.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Post({ post, onClick, noAuthor }) {
+  const date = new Date(post.createdDate);
+  const dateFormatted = `${
+    date.getMonth() + 1
+  }/${date.getDate()}/${date.getFullYear()}`;
+
+  return (
+    <Link
+      to={`/post/${post._id}`}
+      className="list-group-item list-group-item-action"
+      onClick={onClick}
+    >
+      <img className="avatar-tiny" src={post.author.avatar} />{" "}
+      <strong>{post.title}</strong>{" "}
+      <span className="text-muted small">
+        {!noAuthor && <>by {post.author.username}</>} on {dateFormatted}{" "}
+      </span>
+    </Link>
+  );
+}
+
+export default Post;
