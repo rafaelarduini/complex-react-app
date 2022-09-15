a845ca8e640f13313a8f42f8acd0c921f8dddce0
rcavalcanti <Rafael.Cavalcanti@fit-tecnologia.org.br>
2022-09-15 07:38:37 -0300

    Sending & Receiving Chats (Part 1)

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -4,17 +4,44 @@ import { useRef } from "react";
 import { useContext } from "react";
 import DispatchContext from "../DispatchContext";
 import StateContext from "../StateContext";
+import { useImmer } from "use-immer";
 
 function Chat() {
   const chatField = useRef(null);
   const appState = useContext(StateContext);
   const appDispatch = useContext(DispatchContext);
+  const [state, setState] = useImmer({
+    fieldValue: "",
+    chatMessages: []
+  });
 
   useEffect(() => {
     if (appState.isChatOpen) {
       chatField.current.focus();
     }
   }, [appState.isChatOpen]);
+
+  function hadleFieldChange(e) {
+    const value = e.target.value;
+    setState(draft => {
+      draft.fieldValue = value;
+    });
+  }
+
+  function hadleSubmit(e) {
+    e.preventDefault();
+    //Send message to chat server
+
+    setState(draft => {
+      //Add message to state collection of messages
+      draft.chatMessages.push({
+        message: draft.fieldValue,
+        username: appState.user.username,
+        avatar: appState.user.avatar
+      });
+      draft.fieldValue = "";
+    });
+  }
 
   return (
     <div
@@ -33,35 +60,50 @@ function Chat() {
         </span>
       </div>
       <div id="chat" className="chat-log">
-        <div className="chat-self">
-          <div className="chat-message">
-            <div className="chat-message-inner">Hey, how are you?</div>
-          </div>
-          <img
-            className="chat-avatar avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />
-        </div>
-
-        <div className="chat-other">
-          <a href="#">
-            <img
-              className="avatar-tiny"
-              src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"
-            />
-          </a>
-          <div className="chat-message">
-            <div className="chat-message-inner">
-              <a href="#">
-                <strong>barksalot:</strong>
-              </a>
-              Hey, I am good, how about you?
-            </div>
-          </div>
-        </div>
+        {state.chatMessages.map((message, index) => {
+          if (message.username == appState.user.username) {
+            return (
+              <>
+                <div className="chat-self">
+                  <div className="chat-message">
+                    <div className="chat-message-inner">{message.message}</div>
+                  </div>
+                  <img
+                    className="chat-avatar avatar-tiny"
+                    src={message.avatar}
+                  />
+                </div>
+              </>
+            );
+          } else {
+            return (
+              <>
+                <div className="chat-other">
+                  <a href="#">
+                    <img className="avatar-tiny" src={message.avatar} />
+                  </a>
+                  <div className="chat-message">
+                    <div className="chat-message-inner">
+                      <a href="#">
+                        <strong>{message.username}: </strong>
+                      </a>
+                      {message.message}
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          }
+        })}
       </div>
-      <form id="chatForm" className="chat-form border-top">
+      <form
+        onSubmit={hadleSubmit}
+        id="chatForm"
+        className="chat-form border-top"
+      >
         <input
+          value={state.fieldValue}
+          onChange={hadleFieldChange}
           ref={chatField}
           type="text"
           className="chat-field"
