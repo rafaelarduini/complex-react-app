30da4a367dce7ed06ae5d95e96ed03a662a203ad
rcavalcanti <Rafael.Cavalcanti@fit-tecnologia.org.br>
2022-09-16 11:29:45 -0300

    React Suspense - Lazy Loading (part 2)

--- app/Main.js
+++ app/Main.js
@@ -22,8 +22,8 @@ import FlashMessages from "./components/FlashMessages";
 import Profile from "./components/Profile";
 import EditPost from "./components/EditPost";
 import NotFound from "./components/NotFound";
-import Search from "./components/Search";
-import Chat from "./components/Chat";
+const Search = React.lazy(() => import("./components/Search"));
+const Chat = React.lazy(() => import("./components/Chat"));
 import LoadingDotsIcon from "./components/LoadingDotsIcon";
 
 function Main() {
@@ -140,9 +140,13 @@ function Main() {
             classNames="search-overlay"
             unmountOnExit
           >
-            <Search />
+            <div className="search-overlay">
+              <Suspense fallback="">
+                <Search />
+              </Suspense>
+            </div>
           </CSSTransition>
-          <Chat />
+          <Suspense fallback="">{state.loggedIn && <Chat />}</Suspense>
           <Footer />
         </BrowserRouter>
       </DispatchContext.Provider>
