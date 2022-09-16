215b1500368648ef109617447c1fd87eb2146104
rcavalcanti <Rafael.Cavalcanti@fit-tecnologia.org.br>
2022-09-16 14:04:17 -0300

    React Outside of the Browser (Part 1)

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,8 +4,13 @@ import StateContext from "../StateContext";
 import HeaderLoggedIn from "./HeaderLoggedIn";
 import HeaderLoggedOut from "./HeaderLoggedOut";
 
-function Header() {
+function Header(props) {
   const appState = useContext(StateContext);
+  const headerContent = appState.loggedIn ? (
+    <HeaderLoggedIn />
+  ) : (
+    <HeaderLoggedOut />
+  );
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -14,7 +19,7 @@ function Header() {
             ComplexApp
           </Link>
         </h4>
-        {appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}
+        {!props.staticEmpty ? headerContent : ""}
       </div>
     </header>
   );
